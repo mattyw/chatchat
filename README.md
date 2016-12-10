2d8efd629b730eb50ef112632e2df791dbf744dc
mattyw <gh@mattyw.net>
2016-12-10 21:52:01 +0000

    first test

---
+++ README.md
@@ -0,0 +1,12 @@
+# chatchat
+
+1) Run server
+
+2)
+```bash
+nc host port
+```
+
+3) start typing
+
+4) Get friends to follow steps 2-3
--- main.go
+++ main.go
@@ -1,45 +1,58 @@
 package main
 
 import (
-	"io"
+	"bufio"
 	"log"
 	"net"
 	"sync"
+	"time"
 )
 
 type chatRoom struct {
-	participants map[string]io.ReadWriter
+	participants map[string]net.Conn
 	lock         sync.Mutex
+	chatter      [][]byte
 }
 
-func (c *chatRoom) Read(p []byte) (n int, err error) {
-	if len(p) == 0 || string(p) == "\n" {
-		//return 0, nil
+func (c *chatRoom) ReadAll() {
+	for _, conn := range c.participants {
+		go func() {
+			reader := bufio.NewReader(conn)
+			b, err := reader.ReadBytes('\n')
+			if err != nil {
+				panic(err)
+			}
+			c.writeAll(conn.RemoteAddr().String(), b)
+		}()
 	}
-	panic(p)
-	c.lock.Lock()
-	defer c.lock.Unlock()
-	for ra, chatter := range c.participants {
-		_, err := chatter.Write(p)
-		if err != nil {
-			log.Printf("removing chatter %v", ra)
-			delete(c.participants, ra)
+}
+
+func (c *chatRoom) writeAll(sender string, p []byte) {
+	for _, conn := range c.participants {
+		if conn.RemoteAddr().String() == sender {
+			continue
 		}
+		go conn.Write(p)
 	}
-	return len(p), nil
+}
+
+func (c *chatRoom) RunRoom() {
+	for {
+		c.ReadAll()
+		time.Sleep(1e9)
+	}
 }
 
 func (c *chatRoom) AddPerson(p net.Conn) {
-	c.lock.Lock()
-	defer c.lock.Unlock()
 	log.Printf("adding chatter %v", p.RemoteAddr().String())
 	c.participants[p.RemoteAddr().String()] = p
 }
 
 func main() {
 	room := &chatRoom{
-		participants: map[string]io.ReadWriter{},
+		participants: map[string]net.Conn{},
 	}
+	go room.RunRoom()
 	// Listen on TCP port 2000 on all interfaces.
 	l, err := net.Listen("tcp", ":2000")
 	if err != nil {
@@ -58,7 +71,9 @@ func main() {
 		go func(c net.Conn) {
 			// Add to chat room
 			room.AddPerson(c)
-			io.Copy(c, room)
+			for {
+				time.Sleep(10e9)
+			}
 			// Shut down the connection.
 			c.Close()
 		}(conn)
